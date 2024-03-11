1a07d0de25cfdd5c553bb3e6e18de717b3fe4558
4mubarak <muber021@gmail.com>
2024-03-11 11:00:31 +0000

    ✅ Complete task 6

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Mubarak Abdullahi <https://github.com/4mubarak>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
