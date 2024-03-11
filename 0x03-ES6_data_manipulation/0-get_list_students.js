489fb72ef378502521097828a6f952a97c1bc3b9
4mubarak <muber021@gmail.com>
2024-03-11 10:44:19 +0000

    ✅ Complete task 0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,6 +1,6 @@
 /**
  * Retrieves a list of students.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Mubarak Abdullahi <https://github.com/4mubarak>
  * @returns {{id: Number, firstName: String, location: String}[]}
  */
 export default function getListStudents() {
