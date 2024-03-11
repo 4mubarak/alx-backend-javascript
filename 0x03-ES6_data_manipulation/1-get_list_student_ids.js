d21825828dc2f24eb4a921d9bd0087eaa4ad33a6
4mubarak <muber021@gmail.com>
2024-03-11 10:41:12 +0000

    ✅ Complete task 1

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,16 @@
+/**
+ * Retrieves ids from a list of students.
+ * @param {{
+ *   id: Number,
+ *   firstName: String,
+ *   location: String
+ * }[]} students - The list of students.
+ * @author Mubarak Abdullahi <https://github.com/4mubarak>
+ * @returns
+ */
+export default function getListStudentIds(students) {
+  if (students instanceof Array) {
+    return students.map((student) => student.id);
+  }
+  return [];
+}
